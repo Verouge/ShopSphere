a3f101fdb3493f809ab47cfb54a5ff11b883fb34
Verouge <branola1998628@gmail.com>
2023-08-28 20:04:34 +1000

    Added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{handlebars,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
