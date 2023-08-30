fe1c43e9e9aa6218f9e67a1cef9d6dc2192a6c41
AlexanderDuncan1 <hellstern@gmail.com>
2023-08-30 11:55:07 +1000

    seller_functionality

--- routes/au/products/index.js
+++ routes/au/products/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const Product = require('../../../models/Product');
+const auth = require('../../utils/auth');  // Make sure to import your auth middleware
 
 // Create product
-router.post('/create', async (req, res) => {
+router.post('/create', auth, async (req, res) => {
     try {
-        const product = await Product.create(req.body);
+        const product = await Product.create({
+            ...req.body,
+            sellerId: req.session.sellerId  // Set the seller ID from the session
+        });
         res.status(201).json({ message: "Product successfully created!", product });
     } catch (err) {
         console.error(err);
@@ -33,6 +37,73 @@ router.get('/', async (req, res) => {
     }
 });
 
-// ... other CRUD operations ...
+// Get products based on the current logged-in seller
+router.get('/myproducts', auth, async (req, res) => {
+    try {
+        const products = await Product.findAll({ where: { sellerId: req.session.sellerId } });
+        
+        if (!products.length) {
+            res.status(404).json({ message: "No products found." });
+            return;
+        }
+
+        res.json(products);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            message: "Failed to fetch products. Please try again later.",
+            error: err.message
+        });
+    }
+});
+
+// Update product
+router.post('/update/:id', auth, async (req, res) => {
+    try {
+        const updatedProduct = await Product.update(
+            {
+                name: req.body.name,
+                price: req.body.price
+            },
+            {
+                where: {
+                    id: req.params.id,
+                    sellerId: req.session.sellerId  // ensure the product belongs to the seller
+                }
+            }
+        );
+
+        if (updatedProduct[0] === 0) {
+            return res.status(404).json({ message: "Product not found or not updated." });
+        }
+
+        res.json({ message: "Product updated successfully!" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Failed to update product.", error: err.message });
+    }
+});
+
+// Delete product
+router.post('/delete/:id', auth, async (req, res) => {
+    try {
+        const result = await Product.destroy({
+            where: {
+                id: req.params.id,
+                sellerId: req.session.sellerId  // ensure the product belongs to the seller
+            }
+        });
+
+        if (result === 0) {
+            return res.status(404).json({ message: "Product not found." });
+        }
+
+        res.json({ message: "Product deleted successfully!" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Failed to delete product.", error: err.message });
+    }
+});
 
 module.exports = router;
