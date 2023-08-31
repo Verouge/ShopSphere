a784a4dde77428e975df7d0067f374046b88cbeb
Verouge <branola1998628@gmail.com>
2023-08-31 21:29:00 +1000

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,10 @@
 const profileIcon = document.getElementById("profileIcon");
 const loginModal = document.getElementById("loginModal");
 const closeModal = document.getElementById("closeModal");
+const productCarousel = document.getElementById("productCarousel");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+const productWidth = 320; // This should match the width from tailwind classes + margin.
 
 profileIcon.addEventListener("click", function (event) {
   event.preventDefault();
@@ -15,4 +19,14 @@ loginModal.addEventListener("click", function (event) {
   if (event.target === loginModal) {
     loginModal.classList.add("hidden");
   }
+});
+
+nextBtn.addEventListener("click", function () {
+  productCarousel.scrollBy({ left: productWidth, behavior: "smooth" });
+  // Scroll the carousel by the width of one product
+});
+
+prevBtn.addEventListener("click", function () {
+  productCarousel.scrollBy({ left: -productWidth, behavior: "smooth" });
+  // Scroll the carousel backwards by the width of one product
 });
