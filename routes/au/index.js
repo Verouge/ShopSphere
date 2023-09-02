b2cbb45a4eaa320a167fdf610e22ee6f59b68912
Baz Rahimi <bazrahimi@hotmail.com>
2023-09-02 23:04:03 +1000

    fixed Category route issue

--- routes/au/index.js
+++ routes/au/index.js
@@ -5,11 +5,13 @@ const sellersRoutes = require('./sellers');
 const buyersRoutes = require('./buyers');
 const productsRoutes = require('./products');
 const ordersRoutes = require('./orders');
+const categoriesRoutes = require('./categories')
 
 router.use('/sellers', sellersRoutes);
 router.use('/buyers', buyersRoutes);
 router.use('/products', productsRoutes);
 router.use('/orders', ordersRoutes);
+router.use('categories', categoriesRoutes)
 
 
 module.exports = router;
