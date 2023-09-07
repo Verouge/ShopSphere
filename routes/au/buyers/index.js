a63b89d6b365c68b411078e2c9a25be3382bc6e8
Verouge <branola1998628@gmail.com>
2023-09-07 17:14:02 +1000

    Buyerslogin

--- routes/au/buyers/index.js
+++ routes/au/buyers/index.js
@@ -1,40 +1,85 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const Buyer = require("../../../models/Buyer");
 
+router.get("/create-account", (req, res) => {
+  res.render("buyers-signup");
+});
 
-
-const Buyer = require('../../../models/Buyer');
-
-
-// Create Buyer
-router.post('/create', async (req, res) => {
+// Signup
+router.post("/signup", async (req, res) => {
   try {
-      
-      const buyer = await Buyer.create(req.body);
-      res.status(201).json({ message: "Buyer successfully created!", buyer });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal server error", error: err.message });
+    const { username, email, password } = req.body;
 
+    const newBuyer = await Buyer.create({ username, email, password });
+
+    // Save the username to session
+    req.session.username = newBuyer.username;
+
+    res.status(201).json({
+      id: newBuyer.id,
+      username: newBuyer.username,
+      email: newBuyer.email,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error while signing up.", error: error.message });
   }
 });
 
-// Fetch all buyers
-router.get('/', async (req, res) => {
+// Login
+router.post("/login", async (req, res) => {
   try {
-      const buyers = await Buyer.findAll();
-      if (!buyers.length) {
-          res.status(404).json({ message: "No buyers found." });
-          return;
-      }
-      res.json(buyers);
-  } catch (err) {
-      res.status(500).json({ 
-          message: "Failed to fetch buyers. Please try again later.",
-          error: err.message 
+    const { email, password } = req.body;
+
+    const buyer = await Buyer.findOne({ where: { email } });
+    if (!buyer) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password." });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, buyer.password);
+    if (!isValidPassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid email or password." });
+    }
+
+    // Save the username to session
+    req.session.username = buyer.username;
+
+    // Send success response with the username
+    res.json({ success: true, username: buyer.username });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Error while logging in.",
+        error: error.message,
       });
   }
 });
 
-// ... other CRUD operations ...
+// Existing Fetch all buyers
+router.get("/", async (req, res) => {
+  try {
+    const buyers = await Buyer.findAll();
+    if (!buyers.length) {
+      res.status(404).json({ message: "No buyers found." });
+      return;
+    }
+    res.json(buyers);
+  } catch (err) {
+    res.status(500).json({
+      message: "Failed to fetch buyers. Please try again later.",
+      error: err.message,
+    });
+  }
+});
 
 module.exports = router;
